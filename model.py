09f196a90c4a3608a65bc7d9f5080b9114aff8be
Hajime Fujita <hajime.fujita@gmail.com>
2025-02-07 14:54:30 +0900

    各種オシレーターを実装した。

--- model.py
+++ model.py
@@ -40,8 +40,25 @@ class ModelLibrary:
         
     def prepare_training_data(self):
 
-        original_data = self.prepare_original_data()
-        original_data = self.calc_moving_average(original_data)
+        ## 入力データを準備する
+
+        # 元のデータを用意する
+        input_data = self.prepare_original_data()
+
+        # 移動平均を計算する
+        input_data = self.calc_moving_average(input_data)
+
+        # MACDを計算する
+        input_data = self.calc_macd(input_data)
+
+        # ボリンジャーバンドを計算する
+        input_data = self.calc_bollinger_band(input_data)
+
+        # 一目均衡表を計算する
+        input_data = self.calc_ichimoku(input_data)
+
+        # RSIを計算する
+        input_data = self.calc_rsi(input_data)
 
         breakpoint()
 
@@ -97,5 +114,46 @@ class ModelLibrary:
         return original_data
 
 
+    def calc_macd(self, original_data):
 
-    
+        for i in range(self.n_symbols):
+            original_data[i]['MACD'] = original_data[i]['close'].ewm(span=12).mean() - original_data[i]['close'].ewm(span=26).mean()
+            original_data[i]['SIGNAL'] = original_data[i]['MACD'].ewm(span=9).mean()
+            original_data[i]['HISTOGRAM'] = original_data[i]['MACD'] - original_data[i]['SIGNAL']
+
+        return original_data
+
+
+    def calc_bollinger_band(self, original_data):
+        
+        for i in range(self.n_symbols):
+            sma20 = original_data[i]['close'].rolling(window=20).mean()
+            std20 = original_data[i]['close'].rolling(window=20).std()
+            original_data[i]['Upper'] = sma20 + (std20 * 2)
+            original_data[i]['Lower'] = sma20 - (std20 * 2)
+            
+        return original_data
+
+
+    def calc_ichimoku(self, original_data):
+
+        for i in range(self.n_symbols):
+            original_data[i]['ConversionLine'] = (original_data[i]['high'].rolling(window=9).max() + original_data[i]['low'].rolling(window=9).min()) / 2
+            original_data[i]['BaseLine'] = (original_data[i]['high'].rolling(window=26).max() + original_data[i]['low'].rolling(window=26).min()) / 2
+            original_data[i]['LeadingSpanA'] = ((original_data[i]['ConversionLine'] + original_data[i]['BaseLine']) / 2).shift(26)
+            original_data[i]['LeadingSpanB'] = ((original_data[i]['high'].rolling(window=52).max() + original_data[i]['low'].rolling(window=52).min()) / 2).shift(26)
+            original_data[i]['LaggingSpan'] = original_data[i]['close'].shift(-26)
+            
+        return original_data
+
+
+    def calc_rsi(self, original_data):
+        
+        for i in range(self.n_symbols):
+            delta = original_data[i]['close'].diff()
+            gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()
+            loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
+            rs = gain / loss
+            original_data[i]['RSI'] = 100 - (100 / (1 + rs))
+            
+        return original_data
