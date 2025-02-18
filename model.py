b9ad2c62e9ff6600367230c799c2890118d0d39e
Hajime Fujita <hajime.fujita@gmail.com>
2025-02-18 17:36:13 +0900

    さらにバグを修正した。

--- model.py
+++ model.py
@@ -255,7 +255,7 @@ class ModelLibrary:
         for r in raw_data:
             for i in range(len(r) - window):
                 
-                tmp1 = r.drop(['Result'], axis = 1).iloc[i:i + window]
+                tmp1 = r.drop(['Result'], axis = 1).iloc[i:i + window - 1]
                 tmp2 = r.Result.iloc[i + window - 1]
 
                 X = pd.concat([X, pd.DataFrame([tmp1.values.reshape(-1)])])
