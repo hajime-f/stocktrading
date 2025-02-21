9fd425d1391b9221304e5bade6831fe7416aab7c
Hajime Fujita <hajime.fujita@gmail.com>
2025-02-21 20:54:23 +0900

    テストをクラス化した。

--- test.py
+++ test.py
@@ -4,38 +4,67 @@ from library import StockLibrary
 from rich.console import Console
 console = Console(log_time_format = "%Y-%m-%d %H:%M:%S")
 
-if __name__ == '__main__':
+class Test:
 
-    # 株ライブラリを初期化する
-    lib = StockLibrary()
+    def __init__(self, symbol, exchange=1):
 
-    # 登録銘柄リストからすべての銘柄を削除する
-    lib.unregister_all()
+        # 株ライブラリを初期化する
+        self.lib = StockLibrary()
 
-    # 銘柄登録
-    lib.register([1475,])
+        # 登録銘柄リストからすべての銘柄を削除する
+        self.lib.unregister_all()
 
-    # 成行で買い注文を出す
-    content = lib.buy_at_market_price_with_cash(1475, 10)
+        # 銘柄登録
+        self.lib.register(symbol)
 
-    if content['Result'] == 0:
+        # 銘柄情報を取得する
+        content = self.lib.fetch_information(symbol, exchange)
+
+        self.disp_name = content["DisplayName"]
+        self.unit = int(content["TradingUnit"])
         
-        buy_order_id = content['OrderId']
-        console.log(f"[blue]成行で買い注文を出しました[/] \U0001F4B8")
-    
-        # 買い注文の約定状況を確認する
-        result = lib.check_execution(buy_order_id)
+        self.symbol = symbol
+        self.exchange = exchange
         
-        while result['State'] != 5:
-            console.log(f"[blue]買い注文がまだ約定していません[/] \U0001F4B8")
-            result = lib.check_execution(buy_order_id)
-            time.sleep(1)
+        self.buy_price= 0
+        self.sell_price = 0
+
+
+    def buy_stock(self):
+
+        # 成行で買い注文を出す
+        content = self.lib.buy_at_market_price_with_cash(self.symbol, self.unit)
+
+        if content['Result'] == 0:
             
-        console.log(f"[blue]買い注文が約定しました[/] \U0001F4B0")
-        buy_price = result['Price']
-    
+            buy_order_id = content['OrderId']
+            console.log(f"[blue]成行で買い注文を出しました[/] \U0001F4B8")
+        
+            # 買い注文の約定状況を確認する
+            result = self.lib.check_execution(buy_order_id)
+            
+            while result['State'] != 5:
+                console.log(f"[blue]買い注文がまだ約定していません[/] \U0001F4B8")
+                result = self.lib.check_execution(buy_order_id)
+                time.sleep(1)
+                
+            console.log(f"[blue]買い注文が約定しました[/] \U0001F4B0")
+            self.buy_price = result['Price']
+
+            return True
+
+        else:
+
+            console.log(f"[red]買い注文が失敗しました[/] \U0001F6AB")
+            console.log(content)
+
+            return False
+        
+
+    def sell_stock(self):
+
         # 成行で売り注文を出す
-        content = lib.sell_at_market_price_with_cash(1475, 10)
+        content = self.lib.sell_at_market_price_with_cash(self.symbol, self.unit)
 
         if content['Result'] == 0:
 
@@ -43,21 +72,39 @@ if __name__ == '__main__':
             console.log(f"[blue]成行で売り注文を出しました[/] \U0001F4B8")
             
             # 売り注文の約定状況を確認する
-            result = lib.check_execution(sell_order_id)
+            result = self.lib.check_execution(sell_order_id)
             
             while result['State'] != 5:
                 console.log(f"[blue]売り注文がまだ約定していません[/] \U0001F4B8")
-                result = lib.check_execution(sell_order_id)
+                result = self.lib.check_execution(sell_order_id)
                 time.sleep(1)
                 
             console.log(f"[blue]売り注文が約定しました[/] \U0001F4B0")
-            sell_price = result['Price']
+            self.sell_price = result['Price']
 
+            return True
+
+        else:
+
+            console.log(f"[red]売り注文が失敗しました[/] \U0001F6AB")
+            console.log(content)
+
+            return False
+
+
+if __name__ == '__main__':
+    
+    test = Test(1475)
+    
+    if buy_stock():
+        if sell_stock():
+            console.log(f"[green]取引が成功しました[/] \U0001F4B0")
+    
             # 損益を計算する
-            profit = (sell_price - buy_price) * 10
+            profit = (self.sell_price - self.buy_price) * test.unit
             console.log(f"[red]]損益: {profit} 円[/]")
 
-            
+    
         
         
         
