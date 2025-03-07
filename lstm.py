8e2ec5d669a48cc4da5ccad21ee2e80cfdd43e44
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-07 11:30:13 +0900

    ディープラーニングすげえ

--- lstm.py
+++ lstm.py
@@ -16,31 +16,43 @@ from data_management import DataManagement
 pd.set_option("display.max_rows", None)
 
 
-def prepare_input_output_data(df, window=10):
-    X = pd.DataFrame()
-    Y = pd.DataFrame()
+def prepare_input_data(df, window=10):
+    X_list = []
+
+    df = np.array(df)
+    scaler = StandardScaler()
 
     for i in range(len(df) - window):
-        tmp1 = df.drop(["increase"], axis=1).iloc[i : i + window]
-        tmp2 = df["increase"].iloc[i + window - 1]
+        df_s = df[i : i + window]
+        df_std = scaler.fit_transform(df_s)
+        X_list.append(df_std)
 
-        X = pd.concat([X, pd.DataFrame([tmp1.values.reshape(-1)])])
-        Y = pd.concat([Y, pd.DataFrame([tmp2])])
+    return np.array(X_list)
 
-    X = X.iloc[:-1].reset_index(drop=True)
-    Y = Y.iloc[:-1].reset_index(drop=True)
+    # X = pd.DataFrame()
+    # Y = pd.DataFrame()
 
-    return X, Y
+    # for i in range(len(df) - window):
+    #     tmp1 = df.drop(["increase"], axis=1).iloc[i : i + window]
+    #     tmp2 = df["increase"].iloc[i + window - 1]
+
+    #     X = pd.concat([X, pd.DataFrame([tmp1.values.reshape(-1)])])
+    #     Y = pd.concat([Y, pd.DataFrame([tmp2])])
+
+    # X = X.iloc[:-1].reset_index(drop=True)
+    # Y = Y.iloc[:-1].reset_index(drop=True)
+
+    # return X, Y
 
 
 class PredictionModel:
     def __init__(self):
         pass
 
-    def DNN_compile(self, df):
+    def DNN_compile(self, array):
         model = Sequential()
 
-        model.add(InputLayer(shape=(df.shape[1], 1)))
+        model.add(InputLayer(shape=(array.shape[1], array.shape[2])))
         model.add(LSTM(256, activation="relu"))
         model.add(Dropout(0.2))
         model.add(Dense(256, activation="relu"))
@@ -90,47 +102,48 @@ if __name__ == "__main__":
     # 始値と終値の差を追加する
     df["trunk"] = df["open"] - df["close"]
 
-    # nan を削除
-    df = df.dropna()
-
     # 翌営業日の終値が当日より2.5%以上上昇していたらフラグを立てる
     df_shift = df.shift(-1)
     df["increase"] = 0
     df.loc[df_shift["close"] > df["close"], "increase"] = 1
+
+    # nan を削除
+    df = df.dropna()
+
+    # 学習用データとテスト用データに分割
+    df_learn = df[(df.index >= "2021-03-01") & (df.index <= "2024-06-30")]
+    df_test = df[(df.index >= "2024-07-01") & (df.index <= "2025-03-06")]
+
+    # 入出力データを準備
+    window = 10
+    array_learn_X = prepare_input_data(df_learn, window)
+    array_learn_y = df_learn["increase"].iloc[:-window].values
+    array_test_X = prepare_input_data(df_test, window)
+    array_test_y = df_test["increase"].iloc[:-window].values
 
     test_scores = []
     tss = TimeSeriesSplit(n_splits=4)
     pred_model = PredictionModel()
     scaler = StandardScaler()
 
-    window = 10
-    X, y = prepare_input_output_data(df, window)
-    df = df.iloc[: -window - 1]
-
-    for fold, (learn_indices, test_indices) in enumerate(tss.split(df)):
-        X_learn_np_array, X_test_np_array = (
-            X.values[learn_indices],
-            X.values[test_indices],
+    for fold, (learn_indices, test_indices) in enumerate(tss.split(array_learn_X)):
+        array_learn_X_validate, array_test_X_validate = (
+            array_learn_X[learn_indices],
+            array_learn_X[test_indices],
         )
-        y_learn_np_array, y_test_np_array = (
-            y.values[learn_indices],
-            y.values[test_indices],
+        array_learn_y_validate, array_test_y_validate = (
+            array_learn_y[learn_indices],
+            array_learn_y[test_indices],
         )
 
-        X_learn_df = pd.DataFrame(X_learn_np_array)
-        X_test_df = pd.DataFrame(X_test_np_array)
-        y_learn_df = pd.DataFrame(y_learn_np_array)
-        y_test_df = pd.DataFrame(y_test_np_array)
+        model = pred_model.DNN_compile(array_learn_X_validate)
 
-        # X_learn_df = scaler.fit_transform(X_learn_df)
-        # X_test_df = scaler.fit_transform(X_test_df)
-
-        model = pred_model.DNN_compile(X_learn_df)
-
-        model.fit(X_learn_df, y_learn_df, epochs=10, batch_size=64)
-        y_test_pred = model.predict(X_test_df)
+        model.fit(
+            array_learn_X_validate, array_learn_y_validate, epochs=10, batch_size=64
+        )
+        y_test_pred = model.predict(array_test_X_validate)
         y_test_pred = np.where(y_test_pred < 0.5, 0, 1)
-        score = accuracy_score(y_test_df, y_test_pred)
+        score = accuracy_score(array_test_y_validate, y_test_pred)
         print(f"fold {fold} MAE: {score}")
 
         test_scores.append(score)
@@ -138,27 +151,15 @@ if __name__ == "__main__":
     print(f"test_scores: {test_scores}")
     print(f"CV score: {np.mean(test_scores)}")
 
-    # 学習用データを準備する
-    df_learn = df[(df.index >= "2021-03-01") & (df.index <= "2024-06-30")]
-    X_learn = df_learn.drop(columns=["increase"]).iloc[:-1].reset_index(drop=True)
-    y_learn = df_learn["increase"].iloc[:-1].reset_index(drop=True)
-
-    # テスト用データを準備する
-    df_test = df[(df.index >= "2024-07-01") & (df.index <= "2025-03-05")]
-    X_test = df_test.drop(columns=["increase"]).iloc[:-1].reset_index(drop=True)
-    y_test = df_test["increase"].iloc[:-1].reset_index(drop=True)
-
-    # # 標準化
-    # X_learn = scaler.fit_transform(X_learn)
-    # X_test = scaler.transform(X_test)
-
     # モデルの学習
-    model = pred_model.DNN_compile(X_learn)
-    model.fit(X_learn, y_learn, batch_size=64, epochs=10)
+    model = pred_model.DNN_compile(array_learn_X)
+    model.fit(array_learn_X, array_learn_y, batch_size=64, epochs=10)
 
     # モデルの評価
-    y_pred = model.predict(X_test)
-    y_pred = pd.DataFrame((y_pred > 0.5).astype(int))
+    y_pred = model.predict(array_test_X)
+    y_pred = (y_pred > 0.5).astype(int)
 
-    print("accuracy = ", accuracy_score(y_true=y_test, y_pred=y_pred))
-    print(classification_report(y_test, y_pred))
+    print("accuracy = ", accuracy_score(y_true=array_test_y, y_pred=y_pred))
+    print(classification_report(array_test_y, y_pred))
+
+    breakpoint()
