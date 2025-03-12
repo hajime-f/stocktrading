98ca8b12f7311f8f1e3c54784a8a4d650f5b8aa5
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-12 21:02:52 +0900

    修正

--- prediction.py
+++ prediction.py
@@ -8,29 +8,27 @@ from data_management import DataManagement
 pd.set_option("display.max_rows", None)
 
 
-def prepare_input_data(df, window=10):
-    X_list = []
-
-    df = np.array(df)
+def prepare_input_data(df):
+    array = np.array(df)
     scaler = StandardScaler()
 
     try:
-        df_std = scaler.fit_transform(df)
+        array_std = scaler.fit_transform(array)
     except ValueError:
         return None, False
 
-    X_list.append(df_std)
-    return np.array(X_list), True
+    return np.array([array_std]), True
 
 
 if __name__ == "__main__":
     dm = DataManagement()
     stock_list = dm.load_stock_list()
 
-    model = load_model("./model/model_swingtrade_20250310_182001.keras")
+    model = load_model("./model/model_swingtrade_20250312_204936.keras")
+    window = 20
 
     for i, code in enumerate(stock_list["code"]):
-        df = dm.load_stock_data(code).tail(10 * 5)
+        df = dm.load_stock_data(code, start="2025-01-01", end="end")
 
         # 日付をインデックスにする
         df.set_index("date", inplace=True)
@@ -64,17 +62,35 @@ if __name__ == "__main__":
         # 始値と終値の差を追加する
         df["trunk"] = df["open"] - df["close"]
 
+        # 移動平均線乖離率を追加する
+        df["MA5_rate"] = (df["close"] - df["MA5"]) / df["MA5"]
+        df["MA25_rate"] = (df["close"] - df["MA25"]) / df["MA25"]
+
+        # MACDの乖離率を追加する
+        df["MACD_rate"] = (df["MACD"] - df["SIGNAL"]) / df["SIGNAL"]
+
+        # RSIの乖離率を追加する
+        df["RSI_rate"] = (df["RSI"] - 50) / 50
+
+        # ボリンジャーバンドの乖離率を追加する
+        df["Upper_rate"] = (df["close"] - df["Upper"]) / df["Upper"]
+
+        # 移動平均の差を追加する
+        df["MA_diff"] = df["MA5"] - df["MA25"]
+
         # nan を削除
         df = df.dropna()
 
         # データを絞る
-        df = df.tail(10)
+        df = df.tail(window)
+        if len(df) < window:
+            continue
 
-        array_X, flag = prepare_input_data(df, 10)
+        array_X, flag = prepare_input_data(df)
         if not flag:
             continue
         y_pred = model.predict(array_X, verbose=0)
-        y_pred = (y_pred > 0.999).astype(int)
+        y_pred = (y_pred > 0.6).astype(int)
 
         if y_pred:
             print(f"{code}, {stock_list['brand'][i]}")
