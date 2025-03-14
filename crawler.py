f9a867fe8e552a915c015a81a891366c64f806b5
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-15 01:21:28 +0900

    予測値別に結果を出せるようにした

--- crawler.py
+++ crawler.py
@@ -37,22 +37,23 @@ class Crawler:
                     data = [c.text.strip() for c in cells]
                     return data
                 else:
-                    return None, None
+                    return None
 
             else:
-                return None, None
+                return None
 
         else:
-            return None, None
+            return None
 
 
 if __name__ == "__main__":
     dm = DataManagement()
-    symbols = [[symbol[1], symbol[2]] for symbol in dm.fetch_target()]
+    symbols = [[symbol[1], symbol[2], symbol[3]] for symbol in dm.fetch_target()]
 
     total_change = 0
     total_open_price = 0
 
+    # 予測モデルが提案するすべての銘柄を買った場合
     for symbol in symbols:
         crawler = Crawler(symbol[0])
         values = crawler.fetch_stock_data()
@@ -70,4 +71,82 @@ if __name__ == "__main__":
         except ValueError:
             continue
 
-    print(f"{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益")
+    print(f"All：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益")
+
+    total_change = 0
+    total_open_price = 0
+
+    # 予測値が 0.7 以上の銘柄を買った場合
+    for symbol in symbols:
+        if symbol[3] < 0.7:
+            continue
+
+        crawler = Crawler(symbol[0])
+        values = crawler.fetch_stock_data()
+        data = crawler.extract_todays_data(values)
+
+        open_price = float(data[0].replace(",", ""))
+        close_price = float(data[3].replace(",", ""))
+        change = float(data[4])
+        change_p = float(data[5])
+
+        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％")
+        try:
+            total_open_price += open_price
+            total_change += change
+        except ValueError:
+            continue
+
+    print(f"0.7：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益")
+
+    total_change = 0
+    total_open_price = 0
+
+    # 予測値が 0.8 以上の銘柄を買った場合
+    for symbol in symbols:
+        if symbol[3] < 0.8:
+            continue
+
+        crawler = Crawler(symbol[0])
+        values = crawler.fetch_stock_data()
+        data = crawler.extract_todays_data(values)
+
+        open_price = float(data[0].replace(",", ""))
+        close_price = float(data[3].replace(",", ""))
+        change = float(data[4])
+        change_p = float(data[5])
+
+        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％")
+        try:
+            total_open_price += open_price
+            total_change += change
+        except ValueError:
+            continue
+
+    print(f"0.8：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益")
+
+    total_change = 0
+    total_open_price = 0
+
+    # 予測値が 0.9 以上の銘柄を買った場合
+    for symbol in symbols:
+        if symbol[3] < 0.9:
+            continue
+
+        crawler = Crawler(symbol[0])
+        values = crawler.fetch_stock_data()
+        data = crawler.extract_todays_data(values)
+
+        open_price = float(data[0].replace(",", ""))
+        close_price = float(data[3].replace(",", ""))
+        change = float(data[4])
+        change_p = float(data[5])
+
+        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％")
+        try:
+            total_open_price += open_price
+            total_change += change
+        except ValueError:
+            continue
+
+    print(f"0.9：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益")
