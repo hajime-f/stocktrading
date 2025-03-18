fd71979e760fbb651cce95029f885574aaf02fc1
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-18 20:00:19 +0900

    不要なimportを削除

--- update_model.py
+++ update_model.py
@@ -3,10 +3,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.preprocessing import StandardScaler
-
 from keras.models import Sequential
 from keras.layers import Dense, InputLayer, Dropout, SimpleRNN, Bidirectional
 from keras.optimizers import Adam
