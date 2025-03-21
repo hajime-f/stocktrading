de92356987d505ed4145611d2d202b735932215c
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-21 17:06:55 +0900

    処理を高速化

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import urllib.request
+from datetime import datetime
 
+import pandas as pd
 from bs4 import BeautifulSoup
 
 from data_manager import DataManager
@@ -51,111 +53,42 @@ if __name__ == "__main__":
     dm = DataManager()
     symbols = [[symbol[1], symbol[2], symbol[3]] for symbol in dm.fetch_target()]
 
-    total_change = 0
-    total_open_price = 0
+    list_data = []
+
+    for symbol in symbols:
+        crawler = Crawler(symbol[0])
+        values = crawler.fetch_stock_data()
+        data = crawler.extract_todays_data(values)
+
+        open_price = float(data[0].replace(",", ""))
+        close_price = float(data[3].replace(",", ""))
+
+        list_data.append(
+            [
+                datetime.now().strftime("%Y-%m-%d"),
+                symbol[0],
+                symbol[1],
+                open_price,
+                close_price,
+                symbol[2],
+            ]
+        )
+
+    df = pd.DataFrame(
+        list_data, columns=["date", "code", "brand", "open", "close", "pred"]
+    )
+    df["change"] = df["close"] - df["open"]
 
     # 予測モデルが提案するすべての銘柄を買った場合
-    for symbol in symbols:
-        crawler = Crawler(symbol[0])
-        values = crawler.fetch_stock_data()
-        data = crawler.extract_todays_data(values)
+    total = df["change"].sum()
+    print(f"全部：{int(total * 100):,} 円")
 
-        open_price = float(data[0].replace(",", ""))
-        close_price = float(data[3].replace(",", ""))
-        change = float(data[4].replace(",", ""))
-        change_p = float(data[5].replace(",", ""))
-
-        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％：{symbol[2]:.3f}")
-        try:
-            total_open_price += open_price
-            total_change += close_price - open_price
-        except ValueError:
-            continue
-
-    print(
-        f"All：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益\n"
-    )
-
-    total_change = 0
-    total_open_price = 0
-
-    # 予測値が 0.7 以上の銘柄を買った場合
-    for symbol in symbols:
-        if symbol[2] < 0.7:
-            continue
-
-        crawler = Crawler(symbol[0])
-        values = crawler.fetch_stock_data()
-        data = crawler.extract_todays_data(values)
-
-        open_price = float(data[0].replace(",", ""))
-        close_price = float(data[3].replace(",", ""))
-        change = float(data[4].replace(",", ""))
-        change_p = float(data[5].replace(",", ""))
-
-        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％：{symbol[2]:.3f}")
-        try:
-            total_open_price += open_price
-            total_change += close_price - open_price
-        except ValueError:
-            continue
-
-    print(
-        f"0.7：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益\n"
-    )
-
-    total_change = 0
-    total_open_price = 0
-
-    # 予測値が 0.8 以上の銘柄を買った場合
-    for symbol in symbols:
-        if symbol[2] < 0.8:
-            continue
-
-        crawler = Crawler(symbol[0])
-        values = crawler.fetch_stock_data()
-        data = crawler.extract_todays_data(values)
-
-        open_price = float(data[0].replace(",", ""))
-        close_price = float(data[3].replace(",", ""))
-        change = float(data[4].replace(",", ""))
-        change_p = float(data[5].replace(",", ""))
-
-        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％：{symbol[2]:.3f}")
-        try:
-            total_open_price += open_price
-            total_change += close_price - open_price
-        except ValueError:
-            continue
-
-    print(
-        f"0.8：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益\n"
-    )
-
-    total_change = 0
-    total_open_price = 0
+    # 予測値が 0.80 以上の銘柄を買った場合
+    total = df[df["pred"] >= 0.80]["change"].sum()
+    print(f"0.80：{int(total * 100):,} 円")
 
     # 予測値が 0.85 以上の銘柄を買った場合
-    for symbol in symbols:
-        if symbol[2] < 0.85:
-            continue
+    total = df[df["pred"] >= 0.85]["change"].sum()
+    print(f"0.85：{int(total * 100):,} 円")
 
-        crawler = Crawler(symbol[0])
-        values = crawler.fetch_stock_data()
-        data = crawler.extract_todays_data(values)
-
-        open_price = float(data[0].replace(",", ""))
-        close_price = float(data[3].replace(",", ""))
-        change = float(data[4].replace(",", ""))
-        change_p = float(data[5].replace(",", ""))
-
-        print(f"{symbol[0]}：{symbol[1]}, {change}, {change_p}％：{symbol[2]:.3f}")
-        try:
-            total_open_price += open_price
-            total_change += close_price - open_price
-        except ValueError:
-            continue
-
-    print(
-        f"0.85：{int(total_open_price) * 100:,} で {int(total_change) * 100:,} の損益"
-    )
+    dm.save_profit_loss(df)
