4eb44b7f854bf58cea1e91e95adb0b9f7a033d33
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-21 14:09:32 +0900

    いったんコミット

--- stock.py
+++ stock.py
@@ -61,20 +61,16 @@ class Stock:
         約５分間隔で呼ばれる関数
         """
 
-        content = self.lib.fetch_positions(self.symbol, 2)
-        console.log(content)
-        content = self.lib.check_orders(self.symbol, 2)
-        console.log(content)
-        # self.buy_order_id = self.execute_margin_buy_market_order_at_opening()
-        # console.log(self.buy_order_id)
-        # content = self.lib.fetch_positions(self.symbol, 2)
-        # console.log(content)
-        breakpoint()
-
         # 買いポジションを確認する
-        if not self.lib.fetch_positions(self.symbol, 2):
+        buy_position = self.seek_position(side=2)
+        if buy_position is None:
             # 買いポジションがない場合、寄付に信用で成行の買い注文を出す
-            self.buy_order_id = self.execute_margin_buy_market_order_at_opening()
+            self.execute_margin_buy_market_order_at_opening()
+
+        else:
+            # 買いポジションがある場合、約定状況を確認する
+            if len(buy_position["Order_id"]) != 1:
+                raise AssertionError("買いポジションが複数あります")
 
         if self.buy_order_id is not None:
             # 買い注文が出せた場合、約定状況を確認する
@@ -93,7 +89,7 @@ class Stock:
         self.volume = []
 
     def execute_margin_buy_market_order_at_opening(self):
-        # 信用で成行の買い注文を入れる
+        # 寄付に信用で成行の買い注文を入れる
         content = self.lib.execute_margin_buy_market_order_at_opening(
             self.symbol, self.transaction_unit, self.exchange
         )
@@ -103,15 +99,13 @@ class Stock:
         except KeyError:
             console.log(f"{self.disp_name}（{self.symbol}）：[red]発注失敗[/]")
             console.log(content)
-            return None
 
         if result == 0:
             console.log(f"{self.disp_name}（{self.symbol}）：[blue]発注成功[/]")
-            return content["OrderId"]
+            self.save_order(side=2, price=None)
         else:
             console.log(f"{self.disp_name}（{self.symbol}）：[red]発注失敗[/]")
             console.log(content)
-            return None
 
     def check_buy_order_status(self):
         # 買い注文の約定状況を確認する
@@ -127,13 +121,13 @@ class Stock:
 
         return None
 
-    def save_order(self, side):
+    def save_order(self, side, price):
         df_data = pd.DataFrame(
             {
                 "DateTime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "Symbol": self.symbol,
                 "Displayname": self.disp_name,
-                "Price": self.price,
+                "Price": price,
                 "Order_id": self.buy_order_id,
                 "Side": side,
             }
@@ -142,8 +136,18 @@ class Stock:
         self.buy_order_id = None
         self.purchase_price = None
 
+    def seek_position(self, side):
+        df_data = self.dm.load_order()
 
-    def is_buy_order_pending(self):
+        today = datetime.now().strftime("%Y-%m-%d")
+        df_data["DateTime"] = pd.to_datetime(df_data["DateTime"])
+        df_extracted_data = df_data[
+            (df_data["DateTime"].dt.date == pd.to_datetime(today).date())
+            & (df_data["Symbol"] == self.symbol)
+            & (df_data["Side"] == str(side))
+        ]
 
-        
-        
+        if df_extracted_data.empty:
+            return None
+        else:
+            return df_extracted_data
