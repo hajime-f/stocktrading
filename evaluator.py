7e91d6575a806200164be34544e41421bad6e426
Hajime Fujita <hajime.fujita@gmail.com>
2025-03-28 08:21:24 +0900

    全体を関数化

--- evaluator.py
+++ evaluator.py
@@ -21,15 +21,55 @@ pd.set_option("display.max_rows", None)
 pd.options.display.float_format = "{:.6f}".format
 
 
-def compile_model(shape1, shape2):
-    model = Sequential()
+def compile_model(shape1, shape2, model_num):
+    console.log(f"学習モデル：{model_num}")
 
+    model = Sequential()
     model.add(InputLayer(shape=(shape1, shape2)))
-    model.add(Bidirectional(SimpleRNN(200)))
-    model.add(Dropout(0.3))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(0.3))
-    model.add(Dense(1, activation="sigmoid"))
+
+    if model_num == 1:
+        model.add(Bidirectional(SimpleRNN(200)))
+        model.add(Dropout(0.3))
+        model.add(Dense(256, activation="relu"))
+        model.add(Dropout(0.3))
+        model.add(Dense(1, activation="sigmoid"))
+
+    elif model_num == 2:
+        model.add(Bidirectional(LSTM(200)))
+        model.add(Dropout(0.3))
+        model.add(Dense(256, activation="relu"))
+        model.add(Dropout(0.3))
+        model.add(Dense(1, activation="sigmoid"))
+
+    elif model_num == 3:
+        model.add(Conv1D(200, 3, activation=gelu, kernel_regularizer=l2(0.01)))
+        model.add(MaxPooling1D(3))
+        model.add(Conv1D(200, 3, activation=gelu, kernel_regularizer=l2(0.01)))
+        model.add(GlobalMaxPooling1D())
+        model.add(Dense(256, activation="relu"))
+        model.add(Dropout(0.3))
+        model.add(Dense(1, activation="sigmoid"))
+
+    elif model_num == 4:
+        model.add(Conv1D(filters=64, kernel_size=3, activation="relu"))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Dropout(0.3))
+        model.add(LSTM(100, return_sequences=False))
+        model.add(Dropout(0.3))
+        model.add(Dense(100, activation="relu"))
+        model.add(Dropout(0.3))
+        model.add(Dense(1, activation="sigmoid"))
+
+    elif model_num == 5:
+        model.add(Bidirectional(LSTM(128, return_sequences=True)))
+        model.add(Dropout(0.4))
+        model.add(GlobalMaxPooling1D())
+        model.add(Dense(128, activation="relu"))
+        model.add(Dropout(0.4))
+        model.add(Dense(1, activation="sigmoid"))
+
+    else:
+        raise ValueError("model_num が不正です。")
 
     model.compile(
         optimizer=Adam(learning_rate=0.001),
@@ -40,10 +80,11 @@ def compile_model(shape1, shape2):
     return model
 
 
-if __name__ == "__main__":
+def process(model_num):
     dm = DataManager()
     stock_list = dm.load_stock_list()
 
+    console.log("データを読み込んでいます。")
     model = UpdateModel()
     dict_df = {}
 
@@ -51,6 +92,7 @@ if __name__ == "__main__":
         df = dm.load_stock_data(code, start="2024-05-01", end="2024-10-31")
         dict_df[f"{code}"] = model.add_technical_indicators(df)
 
+    console.log("データを処理しています。")
     window = 30
     percentage = 0.5
     list_X, list_y = [], []
@@ -87,7 +129,8 @@ if __name__ == "__main__":
     array_y_test = array_y[num_learn:]
 
     # モデルの学習
-    pred_model = compile_model(array_X.shape[1], array_X.shape[2])
+    console.log("モデルを学習させています。")
+    pred_model = compile_model(array_X.shape[1], array_X.shape[2], model_num)
     pred_model.fit(
         array_X_learn,
         array_y_learn,
@@ -95,9 +138,10 @@ if __name__ == "__main__":
         epochs=30,
         validation_split=0.2,
         callbacks=[EarlyStopping(patience=3)],
+        verbose=0,
     )
 
-    y_pred_proba = pred_model.predict(array_X_test)
+    y_pred_proba = pred_model.predict(array_X_test, verbose=0)
 
     # モデルの評価1
     y_pred = (y_pred_proba > 0.7).astype(int)
@@ -112,6 +156,7 @@ if __name__ == "__main__":
     print(classification_report(array_y_test, y_pred))
 
     # そのモデルで実際にどれくらい儲かるかをバックテストする
+    console.log("データを読み込んでいます。")
     max_row = -1
     dict_df = {}
     for code in stock_list["code"]:
@@ -124,7 +169,7 @@ if __name__ == "__main__":
     list_output = []
 
     for i in range(max_row - window):
-        print(f"{i + 1}/{max_row - window}：データを処理しています。")
+        console.log(f"{i + 1}/{max_row - window}：データを処理しています。")
 
         list_result = []
 
@@ -159,7 +204,7 @@ if __name__ == "__main__":
         )
 
         step = 0.001
-        for i in np.arange(1, 0.85, -step):
+        for i in np.arange(1, 0.70, -step):
             df_extract = df_result.loc[df_result["pred"] >= i, :].copy()
 
             if len(df_extract) == 50:
@@ -185,4 +230,10 @@ if __name__ == "__main__":
 
     console.log(df_output)
     console.log(df_output["result"].mean())
-    breakpoint()
+
+
+if __name__ == "__main__":
+    model_num = [1, 2, 3, 4, 5]
+
+    for n in model_num:
+        process(n)
