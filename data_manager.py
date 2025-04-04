8c4004ea3dc9d18f65e4c25b781a5ce6a8d209bc
Hajime Fujita <hajime.fujita@gmail.com>
2025-04-05 07:42:56 +0900

    DBのカラム名を小文字に変更

--- data_manager.py
+++ data_manager.py
@@ -296,7 +296,7 @@ class DataManager:
         conn = sqlite3.connect(self.db)
         with conn:
             conn.execute(
-                f"UPDATE Orders SET Price = {price} WHERE Order_id = '{order_id}';",
+                f"UPDATE Orders SET price = {price} WHERE order_id = '{order_id}';",
             )
 
     def save_profit_loss(self, df, table_name="ProfitLoss"):
@@ -313,9 +313,9 @@ class DataManager:
                 f"""
                 SELECT *
                 FROM Orders
-                WHERE DATE(DateTime) = date('now', 'localtime')
-                AND Symbol = {symbol}
-                AND Side = {str(side)};
+                WHERE DATE(datetime) = date('now', 'localtime')
+                AND symbol = {symbol}
+                AND side = {str(side)};
                 """,
                 conn,
             )
