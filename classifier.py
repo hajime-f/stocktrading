0d2e7131856bb9b0f046bb406beeaac4163884d7
Hajime Fujita <hajime.fujita@gmail.com>
2025-04-15 07:46:28 +0900

    変数のスケーリング処理を入れた

--- classifier.py
+++ classifier.py
@@ -8,6 +8,19 @@ class Classifier:
         self.dm = DataManager()
         self.df = self.dm.load_aggregate()
 
+    def prepare_data(self):
+        self.df["result"] = (self.df["pl_long"] > self.df["pl_short"]).astype(int)
+
+        # 変数のスケーリング
+        df_tmp = self.df[["threshold", "long", "short"]]
+        df_tmp = (df_tmp - df_tmp.mean()) / df_tmp.std()
+
+        df_input = df_tmp[:-1]
+        df_output = self.df[:-1]["result"]
+        df_test = df_tmp.tail(1)
+
+        return df_input, df_output, df_test
+
     def fit(self, df_input, df_output):
         self.clf = SVC(kernel="linear")
         self.clf.fit(df_input, df_output)
@@ -20,11 +33,9 @@ class Classifier:
 if __name__ == "__main__":
     clf = Classifier()
 
-    clf.df["result"] = (clf.df["pl_long"] > clf.df["pl_short"]).astype(int)
-    df_input = clf.df[:-1][["threshold", "long", "short"]]
-    df_output = clf.df[:-1]["result"]
-    df_test = clf.df.tail(1)[["threshold", "long", "short"]]
+    df_input, df_output, df_test = clf.prepare_data()
 
     clf.fit(df_input, df_output)
     y_pred = clf.predict(df_test)
     print(y_pred)
+    breakpoint()
