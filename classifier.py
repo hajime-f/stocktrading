87c2db9b718a4c2ac86d84201e8031227c5635f0
Hajime Fujita <hajime.fujita@gmail.com>
2025-04-15 07:38:14 +0900

    classifierをクラス化した

--- classifier.py
+++ classifier.py
@@ -5,21 +5,26 @@ from data_manager import DataManager
 
 class Classifier:
     def __init__(self):
-        pass
+        self.dm = DataManager()
+        self.df = self.dm.load_aggregate()
+
+    def fit(self, df_input, df_output):
+        self.clf = SVC(kernel="linear")
+        self.clf.fit(df_input, df_output)
+
+    def predict(self, df_test):
+        y_pred = self.clf.predict(df_test)
+        return y_pred
 
 
 if __name__ == "__main__":
-    dm = DataManager()
+    clf = Classifier()
 
-    df = dm.load_aggregate()
-    df["result"] = (df["pl_long"] > df["pl_short"]).astype(int)
+    clf.df["result"] = (clf.df["pl_long"] > clf.df["pl_short"]).astype(int)
+    df_input = clf.df[:-1][["threshold", "long", "short"]]
+    df_output = clf.df[:-1]["result"]
+    df_test = clf.df.tail(1)[["threshold", "long", "short"]]
 
-    df_input = df[:-1][["threshold", "long", "short"]]
-    df_output = df[:-1]["result"]
-    df_test = df.tail(1)[["threshold", "long", "short"]]
-
-    clf = SVC(kernel="linear")
     clf.fit(df_input, df_output)
-
     y_pred = clf.predict(df_test)
     print(y_pred)
