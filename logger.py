35792422f5b751d586528cc0e126ee7c1acbf705
Hajime Fujita <hajime.fujita@gmail.com>
2025-06-13 09:49:10 +0900

    ロガーを改善

--- logger.py
+++ logger.py
@@ -1,8 +1,35 @@
+from logging import getLogger
 import logging.config
 import yaml
+
+
+class Logger:
+    def __init__(self, path="log_conf.yaml"):
+        with open(path, "rt") as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        self.logger = getLogger(__name__)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
 
 
 if __name__ == "__main__":
     with open("log_conf.yaml", "rt") as f:
         config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
+
+    logger = getLogger(__name__)
+    logger.info("これはテストです。")
