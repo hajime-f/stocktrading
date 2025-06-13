c5ef9f5dfa0b2ba457b01d1197ad7bfae8a93cf9
Hajime Fujita <hajime.fujita@gmail.com>
2025-06-13 09:27:47 +0900

    バグを修正

--- logger.py
+++ logger.py
@@ -1,1 +1,8 @@
+import logging.config
+import yaml
 
+
+if __name__ == "__main__":
+    with open("log_conf.yaml", "rt") as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
