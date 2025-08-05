e7a0fcffaedbb38e9f9986b26848a866e34e51e1
Hajime Fujita <hajime.fujita@gmail.com>
2025-08-05 22:39:43 +0900

    学習・予測のプロセスを元に戻した

--- model_manager.py
+++ model_manager.py
@@ -3,18 +3,21 @@ import sqlite3
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import xgboost as xgb
 from dateutil.relativedelta import relativedelta
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils import shuffle
 from tensorflow.keras import metrics
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Dropout, InputLayer
+from tensorflow.keras.layers import (
+    LSTM,
+    Bidirectional,
+    Dense,
+    Dropout,
+    InputLayer,
+    SimpleRNN,
+)
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 
-from config_manager import cm
 from data_manager import DataManager
 from library import Library
 from misc import Misc
@@ -25,12 +28,7 @@ class ModelManager:
         self.dm = DataManager()
         self.df_stock_list = self.dm.load_stock_list()
 
-        self.lib = Library()
-
-        self.train_split_ratio = float(cm.get("model.train_split_ratio"))
-        self.threshold = float(cm.get("model.threshold"))
-        self.window = int(cm.get("model.window_size"))
-        self.det_per = float(cm.get("model.det_per"))
+        self.window = 30
 
     def add_technical_indicators(self, df):
         # 日付をインデックスにする
@@ -39,18 +37,11 @@ class ModelManager:
         # 移動平均線を追加する
         df["MA5"] = df["close"].rolling(window=5).mean()
         df["MA25"] = df["close"].rolling(window=25).mean()
-        # df["volume_MA20"] = df["volume"].rolling(window=20).mean()
 
-        # 対数変換する
-        df["log_close"] = np.log(df["close"])
-
-        # 差分を取る
-        df["diff"] = df["close"].diff()
-
-        # # MACDを追加する
-        # df["MACD"] = df["close"].ewm(span=12).mean() - df["close"].ewm(span=26).mean()
-        # df["SIGNAL"] = df["MACD"].ewm(span=9).mean()
-        # df["HISTOGRAM"] = df["MACD"] - df["SIGNAL"]
+        # MACDを追加する
+        df["MACD"] = df["close"].ewm(span=12).mean() - df["close"].ewm(span=26).mean()
+        df["SIGNAL"] = df["MACD"].ewm(span=9).mean()
+        df["HISTOGRAM"] = df["MACD"] - df["SIGNAL"]
 
         # ボリンジャーバンドを追加する
         sma20 = df["close"].rolling(window=20).mean()
@@ -69,34 +60,34 @@ class ModelManager:
         df_shift = df.shift(1)
         df["close_rate"] = (df["close"] - df_shift["close"]) / df_shift["close"]
 
-        # # 始値と終値の差を追加する
-        # df["trunk"] = df["open"] - df["close"]
+        # 始値と終値の差を追加する
+        df["trunk"] = df["open"] - df["close"]
 
         # 移動平均線乖離率を追加する
         df["MA5_rate"] = (df["close"] - df["MA5"]) / df["MA5"]
         df["MA25_rate"] = (df["close"] - df["MA25"]) / df["MA25"]
 
-        # # MACDの乖離率を追加する
-        # df["MACD_rate"] = (df["MACD"] - df["SIGNAL"]) / df["SIGNAL"]
+        # MACDの乖離率を追加する
+        df["MACD_rate"] = (df["MACD"] - df["SIGNAL"]) / df["SIGNAL"]
 
-        # # RSIの乖離率を追加する
-        # df["RSI_rate"] = (df["RSI"] - 50) / 50
+        # RSIの乖離率を追加する
+        df["RSI_rate"] = (df["RSI"] - 50) / 50
 
         # ボリンジャーバンドの乖離率を追加する
         df["Upper_rate"] = (df["close"] - df["Upper"]) / df["Upper"]
 
-        # # 移動平均の差を追加する
-        # df["MA_diff"] = df["MA5"] - df["MA25"]
+        # 移動平均の差を追加する
+        df["MA_diff"] = df["MA5"] - df["MA25"]
 
         # nan を削除
         df = df.dropna()
 
         return df
 
-    def compile_model(self, shape1, shape2):
+    def compile_model(self, shape1, shape2, rnn_layer):
         model = Sequential()
         model.add(InputLayer(shape=(shape1, shape2)))
-        model.add(Bidirectional(LSTM(200)))
+        model.add(Bidirectional(rnn_layer))
         model.add(Dropout(0.3))
         model.add(Dense(256, activation="relu"))
         model.add(Dropout(0.3))
@@ -112,242 +103,128 @@ class ModelManager:
 
     def prepare_data(self):
         scaler = StandardScaler()
-
-        dict_df_learn = {}  # 学習用
-        dict_df_test = {}  # テスト用
-        dict_df_close = {}  # 答え
+        dict_df = {}
+        dict_close = {}
 
         today = datetime.date.today()
-        start = (today - relativedelta(months=6)).strftime("%Y-%m-%d")
+        ago = (today - relativedelta(months=4)).strftime("%Y-%m-%d")
 
         for code in self.df_stock_list["code"]:
-            df = self.dm.load_stock_data(code, start=start)
+            df = self.dm.load_stock_data(code, start=ago, end="end")
             df = self.add_technical_indicators(df)
+            dict_df[code] = pd.DataFrame(scaler.fit_transform(df))
+            dict_close[code] = df["close"]
 
-            df_learn = df.iloc[:-1]
-            df_test = df.tail(self.window)
-            dict_df_close[code] = df["close"]
+        return dict_df, dict_close
 
-            train_split_index = int(len(df_learn) * self.train_split_ratio)
-            scaler.fit(df_learn.iloc[:train_split_index])
-
-            dict_df_learn[code] = pd.DataFrame(
-                scaler.transform(df_learn),
-                index=df_learn.index,
-                columns=df_learn.columns,
-            )
-            dict_df_test[code] = pd.DataFrame(
-                scaler.transform(df_test),
-                index=df_test.index,
-                columns=df_test.columns,
-            )
-
-        return dict_df_learn, dict_df_test, dict_df_close
-
-    def evaluate_feature_importance(self, dict_df_learn, dict_df_close, per):
+    def fit(self, dict_df, dict_close, per, opt_model):
         list_X, list_y = [], []
         window = self.window
 
-        original_cols = list(dict_df_learn.values())[0].columns
+        for code in self.df_stock_list["code"]:
+            df = dict_df[code]
+            cl = dict_close[code]
 
-        for code in dict_df_learn.keys():
-            df_scaled = dict_df_learn[code]
-            df_close = dict_df_close[code]
+            for i in range(len(df) - window):
+                list_X.append(df.iloc[i : i + window])
 
-            for i in range(len(df_scaled) - window + 1):
-                window_X = df_scaled.iloc[i : i + window]
+                current_close = cl.iloc[i : i + window].tail(1).item()
+                future_close = cl.iloc[i + window : i + window + 1].item()
 
-                last_date_of_window = window_X.index[-1]
-                loc = df_close.index.get_loc(last_date_of_window)
+                if per > 1:
+                    flag = future_close >= current_close * per
+                elif per <= 1:
+                    flag = future_close <= current_close * per
+                list_y.append(1 if flag else 0)
 
-                current_close = df_close.iloc[loc]
-                future_close = df_close.iloc[loc + 1]
-                label = self.create_label(current_close, future_close, per)
+        array_X = np.array(list_X)
+        array_y = np.array(list_y)
 
-                list_X.append(window_X.to_numpy())
-                list_y.append(label)
-
-        X = np.array(list_X)
-        y = np.array(list_y)
-
-        n_samples, n_timesteps, n_features = X.shape
-        X_reshaped = X.reshape((n_samples, n_timesteps * n_features))
-
-        xgb_model = xgb.XGBClassifier(
-            objective="binary:logistic",
-            eval_metric="logloss",
-            use_label_encoder=False,
-            random_state=42,
-        )
-        xgb_model.fit(X_reshaped, y)
-
-        feature_names = [
-            f"{col}_t-{i}" for i in range(window - 1, -1, -1) for col in original_cols
-        ]
-
-        importances = xgb_model.feature_importances_
-        df_importance = pd.DataFrame(
-            {"feature": feature_names, "importance": importances}
-        )
-
-        df_importance["original_feature"] = df_importance["feature"].apply(
-            lambda x: x.rsplit("_t-", 1)[0]
-        )
-        df_agg_importance = (
-            df_importance.groupby("original_feature")["importance"]
-            .sum()
-            .sort_values(ascending=False)
-        )
-
-        df_plot_data = df_agg_importance.head(50).sort_values(ascending=True)
-
-        plt.style.use("ggplot")
-        fig, ax = plt.subplots(figsize=(10, 12))
-        ax.barh(y=df_plot_data.index, width=df_plot_data.values)
-
-        ax.set_xlabel("Importance")
-        ax.set_ylabel("Feature")
-        plt.tight_layout()
-        plt.show()
-
-    def fit(self, dict_df_learn, dict_df_close, per):
-        list_X_train, list_y_train = [], []
-        list_X_val, list_y_val = [], []
-        window = self.window
-
-        for code in dict_df_learn.keys():
-            df_scaled = dict_df_learn[code]
-            df_close = dict_df_close[code]
-
-            X, y = [], []
-            for i in range(len(df_scaled) - window + 1):
-                window_X = df_scaled.iloc[i : i + window]
-
-                last_date_of_window = window_X.index[-1]
-                loc = df_close.index.get_loc(last_date_of_window)
-
-                current_close = df_close.iloc[loc]
-                future_close = df_close.iloc[loc + 1]
-                label = self.create_label(current_close, future_close, per)
-
-                X.append(window_X)
-                y.append(label)
-
-            split_index = int(len(X) * self.train_split_ratio)
-            list_X_train.extend(X[:split_index])
-            list_y_train.extend(y[:split_index])
-            list_X_val.extend(X[split_index:])
-            list_y_val.extend(y[split_index:])
-
-        X_train, y_train = np.array(list_X_train), np.array(list_y_train)
-        X_val, y_val = np.array(list_X_val), np.array(list_y_val)
-
-        X_train, y_train = shuffle(X_train, y_train, random_state=42)
-
-        model = self.compile_model(X_train.shape[1], X_train.shape[2])
+        # モデルの学習
+        layer = LSTM(200) if opt_model == "lstm" else SimpleRNN(200)
+        model = self.compile_model(array_X.shape[1], array_X.shape[2], layer)
         model.fit(
-            X_train,
-            y_train,
+            array_X,
+            array_y,
             batch_size=128,
             epochs=30,
-            validation_data=(X_val, y_val),
+            validation_split=0.2,
             callbacks=[EarlyStopping(patience=3)],
-            # verbose=0,
+            verbose=0,
         )
 
         return model
 
-    def create_label(self, current_close, future_close, per):
-        if per > 1:
-            flag = future_close >= current_close * per
-        elif per <= 1:
-            flag = future_close <= current_close * per
-        return 1 if flag else 0
-
-    def predict(self, model, dict_df_test, per):
+    def predict(self, model, dict_df):
         list_result = []
+        window = self.window
 
-        for code in dict_df_test.keys():
-            close_price = self.dm.find_newest_close_price(code)
-            if not (700 < close_price < 5500):
-                continue
-
-            array_X = np.array(dict_df_test[code])
+        for code, brand in zip(self.df_stock_list["code"], self.df_stock_list["brand"]):
+            array_X = np.array(dict_df[code].tail(window))
             y_pred = model.predict(np.array([array_X]), verbose=0)
-
-            df = self.df_stock_list
-            brand = df[df["code"] == code]["brand"].iloc[0]
-
             list_result.append([code, brand, y_pred[0][0]])
 
-        result = pd.DataFrame(list_result, columns=["code", "brand", "pred"])
-        return result
+        df_result = pd.DataFrame(list_result, columns=["code", "brand", "pred"])
+        df_extract = df_result[df_result["pred"] >= 0.7].copy()
 
-    def select_candidate(self, df_long, df_short):
-        df_long = df_long[df_long["pred"] >= self.threshold].copy()
-        df_short = df_short[df_short["pred"] >= self.threshold].copy()
-
-        df_long.loc[:, "side"] = 2
-        df_short.loc[:, "side"] = 1
-
-        df = pd.concat([df_long, df_short])
-        df = df.sort_values("pred", ascending=False).drop_duplicates(
-            subset=["code"], keep="first"
-        )
-
-        selected_indices = []
-        for index, row in df.iterrows():
-            if not self.lib.examine_regulation(row["code"]):
-                selected_indices.append(index)
-        df = df.loc[selected_indices, :]
-
-        weights = df["pred"].to_numpy()
-        probabilities = weights / np.sum(weights)
-        sampled_indices = np.random.choice(
-            a=df.index,
-            size=50,
-            replace=False,
-            p=probabilities,
-        )
-        df = df.loc[sampled_indices, ["code", "brand", "pred", "side"]]
-        df = df.sort_values("pred", ascending=False).reset_index()
-
+        # nbd = datetime.date.today().strftime("%Y-%m-%d")
         nbd = Misc.get_next_business_day(datetime.date.today()).strftime("%Y-%m-%d")
-        df.loc[:, "date"] = nbd
-        df = df[["date", "code", "brand", "pred", "side"]]
+        df_extract.loc[:, "date"] = nbd
+        df_extract = df_extract[["date", "code", "brand", "pred"]]
 
-        return df
-
-    def save_result(self, df):
-        conn = sqlite3.connect(self.dm.db)
-        with conn:
-            df.to_sql("Target", conn, if_exists="append", index=False)
+        return df_extract
 
 
 if __name__ == "__main__":
+    # 土日祝日は実行しない
+    if Misc.check_day_type(datetime.date.today()):
+        exit()
+
+    dm = DataManager()
+    lib = Library()
+
     mm = ModelManager()
 
-    # データの準備
-    dict_df_learn, dict_df_test, dict_df_close = mm.prepare_data()
+    # データを準備する
+    dict_df, dict_close = mm.prepare_data()
 
-    # # 特徴量の重要度を評価
-    # mm.evaluate_feature_importance(dict_df_learn, dict_df_close, 1.005)
-    # breakpoint()
+    # ショートモデルを学習する
+    model = mm.fit(dict_df, dict_close, per=0.995, opt_model="lstm")
+    df_short = mm.predict(model, dict_df)
+    df_short.loc[:, "side"] = 1
 
-    # ロングモデルの学習
-    long_model = mm.fit(dict_df_learn, dict_df_close, 1 + mm.det_per)
+    # ロングモデルを学習する
+    model = mm.fit(dict_df, dict_close, per=1.005, opt_model="lstm")
+    df_long = mm.predict(model, dict_df)
+    df_long.loc[:, "side"] = 2
 
-    # ロングモデルの予測
-    df_long = mm.predict(long_model, dict_df_test, 1 + mm.det_per)
+    # 予測結果を統合する
+    df = pd.concat([df_long, df_short])
+    df = df.sort_values("pred", ascending=False).drop_duplicates(
+        subset=["code"], keep="first"
+    )
 
-    # ショートモデルの学習
-    short_model = mm.fit(dict_df_learn, dict_df_close, 1 - mm.det_per)
+    selected_indices = []
 
-    # ショートモデルの予測
-    df_short = mm.predict(short_model, dict_df_test, 1 - mm.det_per)
+    # 不適切な銘柄は除外する
+    for index, row in df.iterrows():
+        close_price = dm.find_newest_close_price(row["code"])
+        if (700 < close_price < 6000) and not lib.examine_regulation(row["code"]):
+            selected_indices.append(index)
+    df = df.loc[selected_indices, :]
 
-    # 最終候補を得る
-    df = mm.select_candidate(df_long, df_short)
-    breakpoint()
-    # 結果を保存する
-    mm.save_result(df)
+    # 予測値に応じて確率的に銘柄を50個サンプリング
+    weights = df["pred"].to_numpy()
+    probabilities = weights / np.sum(weights)
+    sampled_indices = np.random.choice(
+        a=df.index,
+        size=50,
+        replace=False,
+        p=probabilities,
+    )
+    df = df.loc[sampled_indices, ["date", "code", "brand", "pred", "side"]]
+    df = df.sort_values("pred", ascending=False).reset_index()
+
+    conn = sqlite3.connect(dm.db)
+    with conn:
+        df.to_sql("Target", conn, if_exists="append", index=False)
